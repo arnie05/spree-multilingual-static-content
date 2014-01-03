01bb0047676b39e75ac6ec399fb8d9a89a8204f3
khayashida <khayashida@cuon.co.jp>
2014-01-04 01:01:10 +0900

    Update page_decorator.rb

--- app/models/spree/page_decorator.rb
+++ app/models/spree/page_decorator.rb
@@ -1,7 +1,5 @@
-module Spree
-  Page.class_eval do
-    translates :title, :body,
-               :fallbacks_for_empty_translations => true
-    include SpreeI18n::Translatable
-  end
+Spree::Page.class_eval do
+  translates :title, :body, :fallbacks_for_empty_translations => true
+  include SpreeI18n::Translatable
 end
+
