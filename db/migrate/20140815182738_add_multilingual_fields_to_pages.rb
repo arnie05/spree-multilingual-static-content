bbd2d564f262bbff8cd070cc9cf6317a40b670c0
endenis <endenis@gmail.com>
2014-08-15 20:37:43 +0200

    Tiny migration fix

--- db/migrate/20140815182738_add_multilingual_fields_to_pages.rb
+++ db/migrate/20140815182738_add_multilingual_fields_to_pages.rb
@@ -6,6 +6,6 @@ class AddMultilingualFieldsToPages < ActiveRecord::Migration
       :meta_keywords => :string,
       :meta_description => :string
     }
-    Spree::Page.add_translation_fields! fields
+    Spree::Page.add_translation_fields! fields, { migrate_data: true }
   end
 end
