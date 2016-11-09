8656112d10920486c639f075613a819e0ee33369
Arnaud Augier <arnaud@netrezo.com>
2016-11-09 19:03:52 +0100

    add partial to page for translations button

--- app/overrides/render_page_tabs_partial_in_pages_edit.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  :virtual_path => 'spree/pages/edit',
-  :name => 'render_page_tabs_partial_in_pages_edit',
-  :insert_before => 'form',  
-  :text => "
-  <%= render partial: 'page_tabs', locals: {current: :details} %>
-")
